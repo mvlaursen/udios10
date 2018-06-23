e2c4f116ea68b7663d9f2e046231a55caf431a7b
Mike Laursen <mailbox@laursen.org>
2018-06-22 21:58:37 -0700

    At end of Lecture 6, which is simply hooking up a label as outlet and button action.

--- MyFirstApp4Real/MyFirstApp4Real/ViewController.swift
+++ MyFirstApp4Real/MyFirstApp4Real/ViewController.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +21,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func displayText(_ sender: Any) {
+        label.text = "Hello"
+    }
+    
 }
 
