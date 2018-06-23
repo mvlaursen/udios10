6024e549590d07e371451e6cbeb5a2917467614e
Mike Laursen <mailbox@laursen.org>
2018-06-22 21:46:29 -0700

    Rudimentary project for starting Lecture 6

---
+++ MyFirstApp4Real/MyFirstApp4Real/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyFirstApp4Real
+//
+//  Created by Mike Laursen on 6/22/18.
+//  Copyright © 2018 Laursen.org. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
