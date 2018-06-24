6e2ec79e1757ed5ffcc9fc7b21541171ed60a331
Mike Laursen <mailbox@laursen.org>
2018-06-24 16:54:46 -0700

    This is for Lesson 17, and following, concerning how to use text views and fields.

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,10 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var wordArray = ["Apple", "Banana", "Pear", "Lemon"]
+
+for word in wordArray {
+    print(word + " is a nice fruit.")
+}
+
---
+++ Object Functions/Object Functions/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Object Functions
+//
+//  Created by Mike Laursen on 6/24/18.
+//  Copyright © 2018 Laursen.org. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var textView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func setText(_ sender: Any) {
+        label.text = "I am in the label!"
+        
+        textView.text = textField.text
+    }
+}
+
