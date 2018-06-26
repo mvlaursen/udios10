a28780e586dcc83c2ec4d648d49e24e50fd6ef20
Mike Laursen <mailbox@laursen.org>
2018-06-26 13:54:17 -0700

    This lesson has been kinda boring.

--- FadingObjects/FadingObjects/ViewController.swift
+++ FadingObjects/FadingObjects/ViewController.swift
@@ -10,7 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var label2: UILabel!
+    @IBOutlet weak var label3: UILabel!
+    @IBOutlet weak var label4: UILabel!
     @IBOutlet weak var slider: UISlider!
+    
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var switchControl: UISwitch!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +29,42 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func disable(_ sender: Any) {
+        button.isEnabled = false
+        segmentedControl.isEnabled = false
+        switchControl.isEnabled = false
+    }
+    
+    @IBAction func enable(_ sender: Any) {
+        button.isEnabled = true
+        segmentedControl.isEnabled = true
+        switchControl.isEnabled = true
+    }
+    
+    @IBAction func amIDisabled(_ sender: Any) {
+        if button.isEnabled {
+            label4.text = "No"
+        } else {
+            label4.text = "Yes"
+        }
+    }
+    
+    @IBAction func amIHidden(_ sender: Any) {
+        if label2.isHidden {
+            label3.text = "Yes"
+        } else {
+            label3.text = "No"
+        }
+    }
+    
+    @IBAction func hide(_ sender: Any) {
+        label2.isHidden = true
+    }
+    
+    @IBAction func reveal(_ sender: Any) {
+        label2.isHidden = false
+    }
+    
     @IBAction func fadeIn(_ sender: Any) {
         UIView.animate(withDuration: 1.2, animations: {
             self.label.alpha = 1
