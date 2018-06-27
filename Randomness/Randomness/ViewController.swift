02153db020b47b67492169d91a3854bdf9576f36
Mike Laursen <mailbox@laursen.org>
2018-06-27 16:17:11 -0700

    Better

--- Randomness/Randomness/ViewController.swift
+++ Randomness/Randomness/ViewController.swift
@@ -12,6 +12,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceValueLabel: UILabel!
     @IBOutlet weak var statsView: UITextView!
     
+    let NUM_ROLLS: UInt32 = 100
+    let TIME_INTERVAL: TimeInterval = 0.2
+    
     var stats: [UInt32:UInt32] = [:]
     
     override func viewDidLoad() {
@@ -24,23 +27,37 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func rollDice(_ sender: Any) {
-        let diceValue = rollDie() + rollDie()
-        stats[diceValue] = stats[diceValue] ?? 0 + 1
+    @IBAction func rollDiceAction(_ sender: Any) {
+        var rollCount: UInt32 = 0
         
-        diceValueLabel.text = String(diceValue)
-        
-        var statsReport = String()
-        for index:UInt32 in 2...12 {
-            statsReport.append("\(index): ")
-            var column:uint = 0
-            while column < stats[index] ?? 0 {
-                statsReport.append("=")
-                column += 1
+        Timer.scheduledTimer(withTimeInterval: TIME_INTERVAL, repeats: true, block: { (timer) in
+            if rollCount >= self.NUM_ROLLS {
+                timer.invalidate()
+            } else {
+                rollCount += 1
             }
-            statsReport.append("\n")
-        }
-        statsView.text = statsReport
+            
+            let diceValue = self.rollDiceAndCollectStats()
+            
+            self.diceValueLabel.text = String(diceValue)
+            
+            var statsReport = String()
+            for index: UInt32 in 2...12 {
+                statsReport.append("\(index): ")
+                for _: UInt32 in 0...(self.stats[index] ?? 0) {
+                    statsReport.append("=")
+                }
+                statsReport.append("\n")
+            }
+            self.statsView.text = statsReport
+           })
+        
+    }
+    
+    func rollDiceAndCollectStats() -> UInt32 {
+        let diceValue = rollDie() + rollDie()
+        stats[diceValue] = (stats[diceValue] ?? 0) + 1
+        return diceValue
     }
     
     func rollDie() -> UInt32 {
