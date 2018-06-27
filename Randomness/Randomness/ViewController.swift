e1bf6b455bb930c71b988ece350157fc5f071f8d
Mike Laursen <mailbox@laursen.org>
2018-06-27 15:56:51 -0700

    Made the app more Swifty.

--- Randomness/Randomness/ViewController.swift
+++ Randomness/Randomness/ViewController.swift
@@ -9,10 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var result: UILabel!
+    @IBOutlet weak var diceValueLabel: UILabel!
     @IBOutlet weak var statsView: UITextView!
     
-    var stats:Dictionary<UInt32, UInt32> = [2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0, 10:0, 11:0, 12:0]
+    var stats: [UInt32:UInt32] = [:]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,15 +25,16 @@ class ViewController: UIViewController {
     }
     
     @IBAction func rollDice(_ sender: Any) {
-        let u = rollDie() + rollDie()
-        stats[u] = max(0, stats[u]!) + 1
-        result.text = String(u)
+        let diceValue = rollDie() + rollDie()
+        stats[diceValue] = stats[diceValue] ?? 0 + 1
+        
+        diceValueLabel.text = String(diceValue)
         
         var statsReport = String()
         for index:UInt32 in 2...12 {
             statsReport.append("\(index): ")
             var column:uint = 0
-            while column < stats[index]! {
+            while column < stats[index] ?? 0 {
                 statsReport.append("=")
                 column += 1
             }
