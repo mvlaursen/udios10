70d2adecf8f2aa5f3617ae7405d19a95a3ad9e77
Mike Laursen <mailbox@laursen.org>
2018-06-29 11:33:38 -0700

    Practice using Segmented Control

--- SwiftActions/SwiftActions/ViewController.swift
+++ SwiftActions/SwiftActions/ViewController.swift
@@ -9,12 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var segmentedLabel: UILabel!
+    @IBOutlet weak var segmentedOutlet: UISegmentedControl!
     @IBOutlet weak var switchLabel: UILabel!
     @IBOutlet weak var switchOutlet: UISwitch!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        updateSegmentedOutlet()
         updateSwitchOutlet()
     }
 
@@ -23,8 +26,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func segmentedAction(_ sender: Any) {
+        updateSegmentedOutlet()
+    }
+    
     @IBAction func switchAction(_ sender: Any) {
         updateSwitchOutlet()
+    }
+    
+    func updateSegmentedOutlet() {
+        switch segmentedOutlet.selectedSegmentIndex {
+        case 0: segmentedLabel.text = "Segment 1"
+        case 1: segmentedLabel.text = "Segment 2"
+        case 2: segmentedLabel.text = "Segment 3"
+        case 3: segmentedLabel.text = "Segment 4"
+        default: segmentedLabel.text = "Bad segment value"
+        }
     }
     
     func updateSwitchOutlet() {
