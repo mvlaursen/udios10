8db436bb895be3f82458447feb2866af11396f22
Mike Laursen <mailbox@laursen.org>
2018-07-07 13:30:13 -0700

    Improved the implementation.

--- TemperatureCalc/TemperatureCalc/ViewController.swift
+++ TemperatureCalc/TemperatureCalc/ViewController.swift
@@ -9,13 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var enterLabel: UILabel!
-    @IBOutlet weak var formatSeg: UISegmentedControl!
-    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var conversionDirectionSeg: UISegmentedControl!
+    @IBOutlet weak var inputField: UITextField!
+    @IBOutlet weak var inputLabel: UILabel!
     @IBOutlet weak var outputLabel: UILabel!
-    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var thermometerView: UIImageView!
+
+    let DEFAULT_C_ENTRY: String = "23"
+    let DEFAULT_F_ENTRY: String = "73"
     
-    enum ConversionFormat {
+    enum ConversionDirection {
     case celsiusToFahrenheit
     case fahrenheitToCelsius
     }
@@ -23,8 +26,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        formatSeg.selectedSegmentIndex = 0
-        updateViewForFormat(format: .celsiusToFahrenheit)
+        conversionDirectionSeg.selectedSegmentIndex = 0
+        inputField.text = DEFAULT_F_ENTRY
+        updateView(direction: .fahrenheitToCelsius, input: DEFAULT_F_ENTRY)
     }
 
     override func didReceiveMemoryWarning() {
@@ -35,57 +39,64 @@ class ViewController: UIViewController {
     @IBAction func calculate(_ sender: Any) {
         self.resignFirstResponder()
         
-        var c: Double = 0
-        var f: Double = 0
+        var conversionDirection: ConversionDirection = .fahrenheitToCelsius
+        var input: String = DEFAULT_F_ENTRY
 
-        if formatSeg.selectedSegmentIndex == 0 {
-            f = Double(textField.text!) ?? 0
-            c = (5 * (f - 32)) / 9
-            outputLabel.text = String(format: "%4.1f Celsius", c)
-        } else if formatSeg.selectedSegmentIndex == 1 {
-            c = Double(textField.text!) ?? 0
-            f = (9 * c) / 5 + 32
-            outputLabel.text = String(format: "%4.1f Fahrenheit", f)
+        if conversionDirectionSeg.selectedSegmentIndex == 0 {
+            conversionDirection = .fahrenheitToCelsius
+            input = inputField.text ?? DEFAULT_F_ENTRY
+        } else if conversionDirectionSeg.selectedSegmentIndex == 1 {
+            conversionDirection = .celsiusToFahrenheit
+            input = inputField.text ?? DEFAULT_C_ENTRY
         }
         
-        if f < 16 {
-            imageView.image = UIImage(named: "Temp1.png")
-        } else if f < 32 {
-            imageView.image = UIImage(named: "Temp2.png")
-        } else if f < 48 {
-            imageView.image = UIImage(named: "Temp3.png")
-        } else if f < 64 {
-            imageView.image = UIImage(named: "Temp4.png")
-        } else if f < 80 {
-            imageView.image = UIImage(named: "Temp5.png")
-        } else if f < 96 {
-            imageView.image = UIImage(named: "Temp6.png")
-        } else if f < 108 {
-            imageView.image = UIImage(named: "Temp7.png")
-        } else if f < 120 {
-            imageView.image = UIImage(named: "Temp8.png")
-        } else {
-            imageView.image = UIImage(named: "Temp9.png")
-        }
+        updateView(direction: conversionDirection, input: input)
     }
     
     @IBAction func formatAction(_ sender: Any) {
-        if formatSeg.selectedSegmentIndex == 0 {
-            updateViewForFormat(format: .celsiusToFahrenheit)
-        } else if formatSeg.selectedSegmentIndex == 1 {
-            updateViewForFormat(format: .fahrenheitToCelsius)
+        if conversionDirectionSeg.selectedSegmentIndex == 0 {
+            inputField.text = DEFAULT_F_ENTRY
+            updateView(direction: .fahrenheitToCelsius, input: DEFAULT_F_ENTRY)
+        } else if conversionDirectionSeg.selectedSegmentIndex == 1 {
+            inputField.text = DEFAULT_C_ENTRY
+            updateView(direction: .fahrenheitToCelsius, input: DEFAULT_C_ENTRY)
         }
     }
     
-    func updateViewForFormat(format: ConversionFormat) {
-        if (format == .celsiusToFahrenheit) {
-            enterLabel.text = "Enter Fahrenheit"
-            outputLabel.text = "0 Celsius"
-            textField.text = ""
-        } else if (format == .fahrenheitToCelsius) {
-            enterLabel.text = "Enter Celsius"
-            outputLabel.text = "0 Fahrenheit"
-            textField.text = ""
+    func updateView(direction: ConversionDirection, input: String) {
+        var c: Double = 0
+        var f: Double = 0
+        
+        if direction == .celsiusToFahrenheit {
+            c = Double(input) ?? 0
+            f = (9 * c) / 5 + 32
+            inputLabel.text = "Enter Celsius"
+            outputLabel.text = String(format: "%4.1f Fahrenheit", f)
+        } else if direction == .fahrenheitToCelsius {
+            f = Double(input) ?? 0
+            c = (5 * (f - 32)) / 9
+            inputLabel.text = "Enter Fahrenheit"
+            outputLabel.text = String(format: "%4.1f Celsius", c)
+        }
+        
+        if f < 16 {
+            thermometerView.image = UIImage(named: "Temp1.png")
+        } else if f < 32 {
+            thermometerView.image = UIImage(named: "Temp2.png")
+        } else if f < 48 {
+            thermometerView.image = UIImage(named: "Temp3.png")
+        } else if f < 64 {
+            thermometerView.image = UIImage(named: "Temp4.png")
+        } else if f < 80 {
+            thermometerView.image = UIImage(named: "Temp5.png")
+        } else if f < 96 {
+            thermometerView.image = UIImage(named: "Temp6.png")
+        } else if f < 108 {
+            thermometerView.image = UIImage(named: "Temp7.png")
+        } else if f < 120 {
+            thermometerView.image = UIImage(named: "Temp8.png")
+        } else {
+            thermometerView.image = UIImage(named: "Temp9.png")
         }
     }
 }
