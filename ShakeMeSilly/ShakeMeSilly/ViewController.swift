6b5c4e467a8f0985145afe2f56d82e439b816062
Mike Laursen <mailbox@laursen.org>
2018-07-09 13:04:02 -0700

    Everything but the maraca animation?

--- ShakeMeSilly/ShakeMeSilly/ViewController.swift
+++ ShakeMeSilly/ShakeMeSilly/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController {
     
     let COUNTDOWN: Int = 10
     
+    var isPlaying: Bool = false
     var score: Int = 0
     
     override func viewDidLoad() {
@@ -29,11 +30,27 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func becomeFirstResponder() -> Bool {
+        return true
+    }
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        if motion == .motionShake && isPlaying {
+            score += 1
+            scoreLabel.text = String(score)
+        }
+    }
+    
     @IBAction func startGame(_ sender: Any) {
+        startButton.alpha = 0.5
         startButton.isEnabled = false
         
         var countdown = COUNTDOWN
         timerLabel.text = String(countdown)
+        
+        isPlaying = true
+        score = 0
+        scoreLabel.text = "0"
 
         Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { (timer) in
             countdown -= 1
@@ -41,6 +58,8 @@ class ViewController: UIViewController {
             
             if countdown == 0 {
                 timer.invalidate()
+                self.isPlaying = false
+                self.startButton.alpha = 0.75
                 self.startButton.isEnabled = true
             }
         }
