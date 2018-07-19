9cec1436f6a2077de218a8b41403324ba12657f2
Mike Laursen <mailbox@laursen.org>
2018-07-19 12:07:39 -0700

    Practice using Navigation Controller

---
+++ Switcheroo/Switcheroo/FirstViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Switcheroo
+//
+//  Created by Mike Laursen on 7/19/18.
+//  Copyright © 2018 Laursen.org. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+    @IBOutlet weak var button1: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
