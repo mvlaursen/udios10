50091021f5723bb31e1e77b46ad66cd9526049c3
Mike Laursen <mailbox@laursen.org>
2018-07-19 22:27:14 -0700

    Got more web browsing functionality working. Still needs toolbar items to work correctly.

--- WebBrowser/WebBrowser/ViewController.swift
+++ WebBrowser/WebBrowser/ViewController.swift
@@ -7,12 +7,20 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var webView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let url = URL(string: "http://laursen.org/")
+        let request = URLRequest(url: url!)
+        webView.load(request)
     }
 
     override func didReceiveMemoryWarning() {
