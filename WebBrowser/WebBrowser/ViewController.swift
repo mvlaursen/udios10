9c084a8b357ccae9f99e1276cba304441194d258
Mike Laursen <mailbox@laursen.org>
2018-07-19 22:40:26 -0700

    Got the items on the toolbar working.

--- WebBrowser/WebBrowser/ViewController.swift
+++ WebBrowser/WebBrowser/ViewController.swift
@@ -18,7 +18,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        let url = URL(string: "http://laursen.org/")
+        let url = URL(string: "http://yahoo.com/")
         let request = URLRequest(url: url!)
         webView.load(request)
     }
@@ -27,7 +27,25 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func backAction(_ sender: UIBarButtonItem) {
+        if webView.canGoBack {
+            webView.goBack()
+        }
+    }
+    
+    @IBAction func forwardAction(_ sender: UIBarButtonItem) {
+        if webView.canGoForward {
+            webView.goForward()
+        }
+    }
+    
+    @IBAction func refresh(_ sender: UIBarButtonItem) {
+        webView.reload()
+    }
+    
+    @IBAction func stop(_ sender: UIBarButtonItem) {
+        webView.stopLoading()
+    }
 }
 
