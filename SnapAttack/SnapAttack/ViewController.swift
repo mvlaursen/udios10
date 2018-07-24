71bea443b185840b8425acf54dba855249a59f64
Mike Laursen <mailbox@laursen.org>
2018-07-24 15:38:02 -0700

    Use an explicit GameState enum instead of relying on gameMode integer.

--- SnapAttack/SnapAttack/ViewController.swift
+++ SnapAttack/SnapAttack/ViewController.swift
@@ -18,13 +18,14 @@ class ViewController: UIViewController {
     private let CARD_LIST = ["2_of_hearts", "7_of_hearts", "ace_of_hearts"]
     private let COUNTDOWN_START = 20
     
-    private enum GameMode {
+    private enum GameState {
     case ready, playing, ended
     }
     
     private var cardTimer: Timer? = nil
     private var countdown = 0
     private var countdownTimer: Timer? = nil
+    private var gameState = GameState.ready
     private var leftIndex = -1
      private var rightIndex = -1
     private var scoreInt = 0
@@ -42,41 +43,50 @@ class ViewController: UIViewController {
     }
 
     @IBAction func buttonPressed(_ sender: UIButton) {
-        if countdown == COUNTDOWN_START {
+        switch gameState {
+        case .ready:
             button.setTitle("Snap", for: .normal)
-            
+        
             cardTimer = Timer.scheduledTimer(withTimeInterval: 1.2, repeats: true, block: { (_) in
                 self.leftIndex = Int(arc4random_uniform(UInt32(self.CARD_LIST.count)))
                 self.rightIndex = Int(arc4random_uniform(UInt32(self.CARD_LIST.count)))
                 self.leftCard.image = UIImage(named: self.CARD_LIST[self.leftIndex])
                 self.rightCard.image = UIImage(named: self.CARD_LIST[self.rightIndex])
             })
-            
+        
             countdownTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { (_) in
                 self.countdown -= 1
                 self.updateScoreboard()
-                
+            
                 if self.countdown == 0 {
+                    self.gameState = .ended
                     self.cardTimer!.invalidate()
                     self.countdownTimer!.invalidate()
                     self.button.setTitle("Reset", for: .normal)
                 }
             })
-        } else if countdown == 0 {
-            reset()
-        } else {
+            gameState = .playing
+            break
+
+        case .playing:
             // TODO: This isn't quite right. When the user presses the button, we need to stop cardTimer
             // and then reset it.
-            
+        
             if leftIndex == rightIndex {
                 scoreInt += 1
             } else {
                 scoreInt -= 1
             }
+            break
+            
+        case .ended:
+            reset()
+            break
         }
     }
     
     func reset() {
+        gameState = .ready
         button.setTitle("Start", for: .normal)
         leftIndex = -1
         rightIndex = -1
