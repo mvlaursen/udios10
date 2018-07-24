7681228efc1d822a13fe0ecd068f5f2d4f194ff3
Mike Laursen <mailbox@laursen.org>
2018-07-24 13:54:59 -0700

    Implemented the basic countdown timer functionality.

--- SnapAttack/SnapAttack/ViewController.swift
+++ SnapAttack/SnapAttack/ViewController.swift
@@ -18,13 +18,18 @@ class ViewController: UIViewController {
     var cardTimer: Timer? = nil
     var timer: Timer? = nil
     
-    var gameMode = 0
+    var gameMode = 0 // 0 =
     var scoreInt = 0
     var timerInt = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        button.setTitle("Start", for: .normal)
+        scoreInt = 0
+        timerInt = 20
+        updateScoreboard()
     }
 
     override func didReceiveMemoryWarning() {
@@ -33,6 +38,28 @@ class ViewController: UIViewController {
     }
 
     @IBAction func buttonPressed(_ sender: UIButton) {
+        if timerInt == 20 {
+            button.setTitle("Snap", for: .normal)
+            timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { (_) in
+                self.timerInt -= 1
+                self.updateScoreboard()
+                
+                if self.timerInt == 0 {
+                    self.timer!.invalidate()
+                    self.button.setTitle("Reset", for: .normal)
+                }
+            })
+        } else if timerInt == 0 {
+            button.setTitle("Start", for: .normal)
+            scoreInt = 0
+            timerInt = 20
+            updateScoreboard()
+        }
+    }
+    
+    func updateScoreboard() {
+        scoreLabel.text = String("Score: \(scoreInt)")
+        timeLabel.text = String("Time: \(timerInt)")
     }
 }
 
