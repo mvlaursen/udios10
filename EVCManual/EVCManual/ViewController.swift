413abffa099055bad41a0ba0ad189b901e3a0cb3
Mike Laursen <mailbox@laursen.org>
2018-07-25 14:25:16 -0700

    Got the basic functionality working.

---
+++ EVCManual/EVCManual/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  EVCManual
+//
+//  Created by Mike Laursen on 7/25/18.
+//  Copyright © 2018 Laursen.org. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController {
+    @IBOutlet private weak var webView: WKWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let bundleUrl = Bundle.main.bundleURL
+        let manualUrl = bundleUrl.appendingPathComponent("97Eurovan.pdf")
+        let request = URLRequest(url: manualUrl)
+        webView.load(request)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
