f859c88a6743116a8ce46495a70a7bb8f148d446
Mike Laursen <mailbox@laursen.org>
2018-07-26 17:41:52 -0700

    Generated the basic setup for a collection view.

---
+++ ImageGallery/ImageGallery/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  ImageGallery
+//
+//  Created by Mike Laursen on 7/26/18.
+//  Copyright © 2018 Laursen.org. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
