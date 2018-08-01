a841491c64517d5c43b40d3804edecb45555a55d
Mike Laursen <mailbox@laursen.org>
2018-08-01 11:59:54 -0700

    Basic implementation. Needs some improvement.

--- PetSounds/PetSounds/ViewController.swift
+++ PetSounds/PetSounds/ViewController.swift
@@ -6,9 +6,11 @@
 //  Copyright © 2018 Laursen.org. All rights reserved.
 //
 
+import AVFoundation
 import UIKit
 
 class ViewController: UIViewController {
+    var player: AVAudioPlayer? = nil
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +22,28 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func playSound(_ sender: UIButton) {
+        if player != nil {
+            player!.stop()
+            player = nil
+        }
+        
+        let id = sender.restorationIdentifier
+        
+        assert(id != nil)
+        if (id != nil) {
+            let path = Bundle.main.path(forResource: id, ofType: nil)
+            
+            assert(path != nil)
+            if path != nil {
+                do {
+                    player = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path!))
+                    player?.play()
+                } catch {
+                    print(error)
+                }
+            }
+        }
+    }
 }
 
