f7faf874243cfbdeb8ffe4f9ed15894690d9bdc1
Mike Laursen <mailbox@laursen.org>
2018-08-03 12:40:18 -0700

    Checkpointing generated project.

---
+++ LondonLandscaping/LondonLandscaping/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  LondonLandscaping
+//
+//  Created by Mike Laursen on 8/3/18.
+//  Copyright © 2018 Laursen.org. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
