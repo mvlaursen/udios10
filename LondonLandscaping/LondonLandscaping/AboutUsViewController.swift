924cb9bee7a9745209c6aca95318dd81c777e9e0
Mike Laursen <mailbox@laursen.org>
2018-08-03 13:27:01 -0700

    Gave more meaningful names to FirstViewController and SecondViewController.

--- LondonLandscaping/LondonLandscaping/AboutUsViewController.swift
+++ LondonLandscaping/LondonLandscaping/AboutUsViewController.swift
@@ -1,5 +1,5 @@
 //
-//  FirstViewController.swift
+//  SecondViewController.swift
 //  LondonLandscaping
 //
 //  Created by Mike Laursen on 8/3/18.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class AboutUsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
--- LondonLandscaping/LondonLandscaping/HomeViewController.swift
+++ LondonLandscaping/LondonLandscaping/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  SecondViewController.swift
+//  FirstViewController.swift
 //  LondonLandscaping
 //
 //  Created by Mike Laursen on 8/3/18.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
