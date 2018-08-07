9a1b9099222b3b342c242984600faf73df71eb82
Mike Laursen <mailbox@laursen.org>
2018-08-07 16:01:26 -0700

    More setup of Social Links views.

---
+++ LondonLandscaping/LondonLandscaping/Social/SocialCell.swift
@@ -0,0 +1,24 @@
+//
+//  SocialCell.swift
+//  LondonLandscaping
+//
+//  Created by Mike Laursen on 8/7/18.
+//  Copyright © 2018 Laursen.org. All rights reserved.
+//
+
+import UIKit
+
+class SocialCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
