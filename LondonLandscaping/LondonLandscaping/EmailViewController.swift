ef0ec62af24e78e07d076f9280a09db54e5313b4
Mike Laursen <mailbox@laursen.org>
2018-08-06 17:35:15 -0700

    Checkpointing for now. Working on email view.

---
+++ LondonLandscaping/LondonLandscaping/EmailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  EmailViewController.swift
+//  LondonLandscaping
+//
+//  Created by Mike Laursen on 8/6/18.
+//  Copyright © 2018 Laursen.org. All rights reserved.
+//
+
+import UIKit
+
+class EmailViewController: UIViewController {
+    @IBOutlet weak var emailAddressField: UITextField!
+    @IBOutlet weak var messageField: UITextView!
+    @IBOutlet weak var nameField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func send(_ sender: UIButton) {
+    }
+}
