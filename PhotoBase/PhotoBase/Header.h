1192083f5e965fe9059b598932ad21d3b183ccfa
Mike Laursen <mailbox@laursen.org>
2018-08-23 10:47:59 -0700

    Created table view-related classes.

---
+++ PhotoBase/PhotoBase/Header.h
@@ -0,0 +1,13 @@
+//
+//  Header.h
+//  PhotoBase
+//
+//  Created by Mike Laursen on 8/22/18.
+//  Copyright © 2018 Laursen.org. All rights reserved.
+//
+
+#ifndef Header_h
+#define Header_h
+
+
+#endif /* Header_h */
---
+++ PhotoBase/PhotoBase/PBTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PBTableViewCell.swift
+//  PhotoBase
+//
+//  Created by Mike Laursen on 8/22/18.
+//  Copyright © 2018 Laursen.org. All rights reserved.
+//
+
+import UIKit
+
+class PBTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
