c8e5498704b08a9bf9d0a15956a803c081b06bad
Mike Laursen <mailbox@laursen.org>
2018-08-28 10:42:01 -0700

    Set up the basic app.

---
+++ SocialSharing/SocialSharing/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  SocialSharing
+//
+//  Created by Mike Laursen on 8/28/18.
+//  Copyright © 2018 Laursen.org. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet private weak var imageView: UIImageView!
+    @IBOutlet private weak var label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction private func share(_ sender: UIButton) {
+    }
+}
+
