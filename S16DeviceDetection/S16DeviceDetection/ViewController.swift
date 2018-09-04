9dce025a967a76dcf2e15f00658763e8cce4d728
Mike Laursen <mailbox@laursen.org>
2018-09-04 15:53:47 -0700

    Well, that's that.

--- S16DeviceDetection/S16DeviceDetection/ViewController.swift
+++ S16DeviceDetection/S16DeviceDetection/ViewController.swift
@@ -9,8 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet private weak var imageView: UIImageView!
-    @IBOutlet private weak var label: UILabel!
+    @IBOutlet private weak var deviceImageView: UIImageView!
+    @IBOutlet private weak var deviceTypeLabel: UILabel!
+    @IBOutlet private weak var screenWidth: UILabel!
+    @IBOutlet private weak var screenHeight: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,13 +27,50 @@ class ViewController: UIViewController {
     @IBAction private func whatKindOfDeviceAmI(_ sender: UIButton) {
         switch UIDevice.current.userInterfaceIdiom {
         case .pad:
-            label.text = "I am an iPad."
-            imageView.image = UIImage(named: "ipad")
+            deviceTypeLabel.text = "I am an iPad."
+            deviceImageView.image = UIImage(named: "ipad")
         case .phone:
-            label.text = "I am an iPhone."
-            imageView.image = UIImage(named: "iphone")
+            deviceTypeLabel.text = "I am an iPhone."
+            deviceImageView.image = UIImage(named: "iphone")
         default:
+            deviceTypeLabel.text = "I am neither an iPad nor an iPhone."
+            deviceImageView.image = nil
             break
+        }
+        
+        let screenSize = UIScreen.main.bounds
+        screenWidth.text = "\(Int(screenSize.width))"
+        screenHeight.text = "\(Int(screenSize.height))"
+        
+        // iPhone 2G - 5S
+        if screenSize.width == 320 {
+            if screenSize.height == 480 {
+                deviceTypeLabel.text = "Am I an iPhone 2G, 3G, 3GS, 4, or 4s?"
+            } else if screenSize.height == 568 {
+                deviceTypeLabel.text = "Am I an iPhone 5, 5s, 5c, or SE?"
+            }
+        } else if screenSize.width == 375 {
+            if screenSize.height == 667 {
+                deviceTypeLabel.text = "Am I an iPhone 6, 6s, 7, or 8?"
+            } else if screenSize.height == 812 {
+                deviceTypeLabel.text = "Am I an iPhone X?"
+            }
+        } else if screenSize.width == 414 {
+            if screenSize.height == 736 {
+                deviceTypeLabel.text = "Am I an iPhone 6 Plus, 6s Plus, 7 Plus, or 8 Plus?"
+            }
+        } else if screenSize.width == 768 {
+            if screenSize.height == 1024 {
+                deviceTypeLabel.text = "Am I an iPad mini, Air, or Pro 9.7\"?"
+            }
+        } else if screenSize.width == 834 {
+            if screenSize.height == 1112 {
+                deviceTypeLabel.text = "Am I an iPad Pro 10.5\"?"
+            }
+        } else if screenSize.width == 1024 {
+            if screenSize.height == 1366 {
+                deviceTypeLabel.text = "Am I an iPad Pro 12.9\"?"
+            }
         }
     }
 }
