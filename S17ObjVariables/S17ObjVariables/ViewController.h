cb2814b9f5e4b93e59aeffcde7b1db2d66995caa
Mike Laursen <mailbox@laursen.org>
2018-09-05 10:48:57 -0700

    Completed exercise

--- S17ObjVariables/S17ObjVariables/ViewController.h
+++ S17ObjVariables/S17ObjVariables/ViewController.h
@@ -9,16 +9,5 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
-
-// TODO: Experiment with moving these to .m so they are "private".
-
-@property (weak, nonatomic) IBOutlet UILabel *label1;
-@property (weak, nonatomic) IBOutlet UILabel *label2;
-@property (weak, nonatomic) IBOutlet UILabel *label3;
-
-@property (weak, nonatomic) IBOutlet UIButton *button;
-
-@property (weak, nonatomic) IBOutlet UISwitch *switchOutlet;
-
 @end
 
--- S17ObjVariables/S17ObjVariables/ViewController.m
+++ S17ObjVariables/S17ObjVariables/ViewController.m
@@ -9,7 +9,13 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *label1;
+@property (weak, nonatomic) IBOutlet UILabel *label2;
+@property (weak, nonatomic) IBOutlet UILabel *label3;
 
+@property (weak, nonatomic) IBOutlet UIButton *button;
+
+@property (weak, nonatomic) IBOutlet UISwitch *switchOutlet;
 @end
 
 @implementation ViewController
@@ -17,9 +23,22 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
-    NSString *string1 = @"Hello";
-    NSString *string2 = @"world";
-    self.label1.text = [NSString stringWithFormat:@"%@, %@", string1, string2];
+    NSString *s1 = @"Hello";
+    NSString *s2 = @"world";
+    self.label1.text = [NSString stringWithFormat:@"%2$@, %1$@", s2, s1];
+    
+    int i = 42;
+    int j = 13;
+    self.label2.text = [NSString stringWithFormat:@"%d", i + j];
+    
+    double f1 = 123.456;
+    double f2 = 667.321;
+    self.label3.text = [NSString stringWithFormat:@"%.2f %.2f", f1, f2];
+    
+    bool b1 = false;
+    bool b2 = NO;
+    self.button.enabled = b1;
+    self.switchOutlet.on = b2;
 }
 
 
